dcd43032626bd3831b154df2c37ba51986afd016
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2016-06-29 18:55:06 +0200

    Create package for nex kata (Minesweeper)

---
+++ katas/src/main/java/com/wouter/kata/minesweeper/MineSweeper.java
@@ -0,0 +1,7 @@
+package com.wouter.kata.minesweeper;
+
+/**
+ * Created by Wouter on 29/06/2016.
+ */
+public class MineSweeper {
+}
---
+++ katas/src/test/java/com/wouter/kata/minesweeper/MineSweeperTest.java
@@ -0,0 +1,16 @@
+package com.wouter.kata.minesweeper;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Wouter on 29/06/2016.
+ */
+public class MineSweeperTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
